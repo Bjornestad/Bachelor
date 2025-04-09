c5588d36f1c8befb7bcba39281d0e91bdc8e7306
Yalcin Acili <yalcinacili@gmail.com>
2025-04-10 01:18:19 +0200

    Fix mac impl

--- Services/InputService.cs
+++ Services/InputService.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading;
+using System.Runtime.InteropServices;
 using Avalonia.Input;
 using InputSimulatorStandard;
 using InputSimulatorStandard.Native;
@@ -12,25 +12,46 @@ public class InputService
 {
     private static readonly InputSimulator _simulator = new InputSimulator();
     private Dictionary<string, bool> _keysCurrentlyDown = new Dictionary<string, bool>();
-    private readonly TimeSpan _keyPressTimeout = TimeSpan.FromMilliseconds(200);
-    
+
+    // macOS CoreGraphics P/Invoke declarations
+    [DllImport("/System/Library/Frameworks/CoreGraphics.framework/CoreGraphics")]
+    private static extern IntPtr CGEventCreateKeyboardEvent(IntPtr source, ushort keyCode, bool keyDown);
+
+    [DllImport("/System/Library/Frameworks/CoreGraphics.framework/CoreGraphics")]
+    private static extern void CGEventPost(uint tapLocation, IntPtr eventRef);
+
+    [DllImport("/System/Library/Frameworks/CoreGraphics.framework/CoreGraphics")]
+    private static extern void CFRelease(IntPtr cf);
+
+    // Constants for CGEventPost
+    private const uint kCGHIDEventTap = 0;
+
     public void SimulateKeyDown(string keyName, string movementName)
     {
         if (Enum.TryParse<Key>(keyName, out var key))
         {
-            VirtualKeyCode vkCode = MapAvaloniaKeyToVirtualKey(key);
             string keyIdentifier = $"{movementName}:{keyName}";
-            
-            // If key isn't already being held down, press it
+
             if (!_keysCurrentlyDown.ContainsKey(keyIdentifier) || !_keysCurrentlyDown[keyIdentifier])
             {
                 Console.WriteLine($"Pressing key: {keyName} | Movement: {movementName}");
-                _simulator.Keyboard.KeyDown(vkCode);
+
+                if (OperatingSystem.IsWindows())
+                {
+                    VirtualKeyCode vkCode = MapAvaloniaKeyToVirtualKey(key);
+                    _simulator.Keyboard.KeyDown(vkCode);
+                }
+                else if (OperatingSystem.IsMacOS())
+                {
+                    ushort macKeyCode = MapAvaloniaKeyToMacKeyCode(key);
+                    SimulateMacKeyEvent(macKeyCode, true);
+                }
+
                 _keysCurrentlyDown[keyIdentifier] = true;
             }
         }
     }
-    
+
     public void ReleaseKey(string keyName, string movementName)
     {
         if (Enum.TryParse<Key>(keyName, out var key))
@@ -38,14 +59,23 @@ public class InputService
             string keyIdentifier = $"{movementName}:{keyName}";
             if (_keysCurrentlyDown.ContainsKey(keyIdentifier) && _keysCurrentlyDown[keyIdentifier])
             {
-                VirtualKeyCode vkCode = MapAvaloniaKeyToVirtualKey(key);
-                _simulator.Keyboard.KeyUp(vkCode);
+                if (OperatingSystem.IsWindows())
+                {
+                    VirtualKeyCode vkCode = MapAvaloniaKeyToVirtualKey(key);
+                    _simulator.Keyboard.KeyUp(vkCode);
+                }
+                else if (OperatingSystem.IsMacOS())
+                {
+                    ushort macKeyCode = MapAvaloniaKeyToMacKeyCode(key);
+                    SimulateMacKeyEvent(macKeyCode, false);
+                }
+
                 _keysCurrentlyDown[keyIdentifier] = false;
                 Console.WriteLine($"Releasing key: {keyName} | Movement: {movementName}");
             }
         }
     }
-    
+
     public void ReleaseAllKeys()
     {
         foreach (var kvp in _keysCurrentlyDown.ToList())
@@ -54,21 +84,45 @@ public class InputService
             {
                 string[] parts = kvp.Key.Split(':');
                 if (parts.Length != 2) continue;
-                
+
                 string movementName = parts[0];
                 string keyName = parts[1];
-                
-                if (Enum.TryParse<Key>(keyName, out var key))
-                {
-                    VirtualKeyCode vkCode = MapAvaloniaKeyToVirtualKey(key);
-                    _simulator.Keyboard.KeyUp(vkCode);
-                    _keysCurrentlyDown[kvp.Key] = false;
-                }
+
+                ReleaseKey(keyName, movementName);
             }
         }
         Console.WriteLine("Released all keys");
     }
-    
+
+    private void SimulateMacKeyEvent(ushort keyCode, bool keyDown)
+    {
+        IntPtr eventRef = CGEventCreateKeyboardEvent(IntPtr.Zero, keyCode, keyDown);
+        CGEventPost(kCGHIDEventTap, eventRef);
+        CFRelease(eventRef);
+    }
+
+    private ushort MapAvaloniaKeyToMacKeyCode(Key key)
+    {
+        // Map Avalonia Keys to macOS Virtual Key Codes
+        // https://developer.apple.com/documentation/coreservices/1390584-keycode
+        return key switch
+        {
+            Key.A => 0,      // kVK_ANSI_A
+            Key.S => 1,      // kVK_ANSI_S
+            Key.D => 2,      // kVK_ANSI_D
+            Key.W => 13,     // kVK_ANSI_W
+            Key.Q => 12,     // kVK_ANSI_Q
+            Key.E => 14,     // kVK_ANSI_E
+            Key.Up => 126,   // kVK_UpArrow
+            Key.Down => 125, // kVK_DownArrow
+            Key.Left => 123, // kVK_LeftArrow
+            Key.Right => 124,// kVK_RightArrow
+            Key.Space => 49, // kVK_Space
+            Key.Enter => 36, // kVK_Return
+            _ => 49          // Default to space
+        };
+    }
+
     private VirtualKeyCode MapAvaloniaKeyToVirtualKey(Key key)
     {
         switch (key)
