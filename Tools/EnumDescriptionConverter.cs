ce449178681b0808d89f5677254fe002e264d8cd
Bjornestad <frederikbjornestad@gmail.com>
2025-04-30 19:47:15 +0200

    Tools folder didnt get pushed, fixed gitignore

---
+++ Tools/EnumDescriptionConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+
+namespace Bachelor.Tools
+{
+    public class EnumDescriptionConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is Enum enumValue ? enumValue.GetDescription() : value?.ToString() ?? string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}
---
+++ Tools/ImagePathConverter.cs
@@ -0,0 +1,34 @@
+﻿using Avalonia;
+using Avalonia.Data.Converters;
+using Avalonia.Platform;
+using System;
+using System.Globalization;
+
+namespace Bachelor.Converters
+{
+    public class ImagePathConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string path && !string.IsNullOrEmpty(path))
+            {
+                try
+                {
+                    var uri = new Uri(path);
+                    return new Avalonia.Media.Imaging.Bitmap(AssetLoader.Open(uri));
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Failed to load image {path}: {ex.Message}");
+                    return null;
+                }
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
